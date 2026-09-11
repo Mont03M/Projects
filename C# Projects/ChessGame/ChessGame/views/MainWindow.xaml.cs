c4d0612b878df375349463d2ef990165d5466122
Mont03M <devops.storm@gmail.com>
2026-09-11 19:11:12 -0400

    Added comments

--- C# Projects/ChessGame/ChessGame/views/MainWindow.xaml.cs
+++ C# Projects/ChessGame/ChessGame/views/MainWindow.xaml.cs
@@ -27,13 +27,20 @@ namespace ChessGame
             this.appSettingsService = appSettingsService;
         }
 
+        /// <summary>
+        /// Handles the window closing event. Disposes of the StockFishEval object if it exists and saves application settings asynchronously.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
         private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            // Dispose of the StockFishEval object if it exists
             if (SwitchControl.Content is ChessMatch chessMatch && chessMatch.DataContext is ChessMatchVM vm)
             {
                 vm.ChessGame.StockFishEval?.Dispose();
             }
 
+            // Save application settings asynchronously
             await this.appSettingsService.SaveAsync();
         }
     }
